767e13886867fc6600a93faa8879b74e18633091
nfherrin <nfherrin@ncsu.edu>
2022-10-21 20:20:01 -0400

    Added output of extrapolation results

--- src/output_module.f90
+++ src/output_module.f90
@@ -7,6 +7,17 @@ MODULE output_module
 CONTAINS
 
   SUBROUTINE output_rich()
+    INTEGER :: i
+
+    WRITE(*,'(A)')'Extrapolation successful for at least 1 point!'
+    WRITE(*,'(A)')'----------------------------------------------'
+    WRITE(*,'(A)')'   Extrapolation Result  | Data Points Used'
+    DO i=1,data_size
+      IF(ABS(rich_results(i)) .GE. 1.0D-15)THEN
+        WRITE(*,'(ES24.16,A,I0,A,I0,A,I0)')rich_results(i),' | ',i-2,' ',i-1,' ',i
+      ENDIF
+    ENDDO
+    WRITE(*,'(A)')'----------------------------------------------'
   ENDSUBROUTINE output_rich
 
 ENDMODULE output_module
